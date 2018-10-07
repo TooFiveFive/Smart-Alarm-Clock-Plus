4dd1d7f4637dd6ccf977f6cc5562cbfd341b3c3e
TooFiveFive <e.moynihan00@gmail.com>
2018-10-07 21:44:47 +0100

    added tts

--- dependencies.sh
+++ dependencies.sh
@@ -2,6 +2,7 @@ sudo apt-get install portaudio19-dev
 sudo apt-get install python3-pyaudio
 pip3 install pyaudio
 pip3 install SpeechRecognition
+pip3 install gTTS
 python3 -m pip install -U mypy
 
 cd ZeroSeg/
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import speech_recognition as sr
 import time
+from gtts import gTTS
+import os
 import threading
+import sys
 from env import WIT_AI_KEY
+
+sys.stderr = object
+
 
 def handler(response):
 
     if response.startswith("clock"):
-        print ("yes?")
+        tts = gTTS(text='Yes?', lang='en')
+        tts.save("hey.mp3")
+        os.system("mpg123 hey.mp3")
+        os.system("rm hey.mp3")
 
 
 def speech():
@@ -17,7 +26,6 @@ def speech():
 
         print("Say something!")
         audio = r.listen(source)
-        
 
     try:
 
@@ -38,6 +46,7 @@ def speech():
 
 def run():
     print("yo") 
+
 
 thread1 = threading.Thread(name='speech', target=speech)
 thread2 = threading.Thread(name='run', target=run)
